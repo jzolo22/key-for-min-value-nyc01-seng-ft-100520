85c8f2b0f5425042228765b2b46f15c750a8371a
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-14 18:59:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, amount|
     if amount < count
       min_value_name = name
-      count = value
+      count = amount
     end
   end
   min_value_name
